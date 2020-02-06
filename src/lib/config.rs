658189555b36a2bf6d75c0b789546d5d8b143a8d
Sendy Halim <sendyhalim93@gmail.com>
2020-02-07 01:29:41 +0700

    Add logic to read and persist config

--- src/lib/config.rs
+++ src/lib/config.rs
@@ -1,12 +1,84 @@
-use dirs;
+use std::collections::HashMap;
+use std::fs;
 use std::path::PathBuf;
 
-pub struct ProjectConfig {
-  pub db_uri: String,
-  pub project_git_path: PathBuf,
+use dirs;
+use failure::Fail;
+use serde::Deserialize;
+use serde::Serialize;
+
+use crate::types::ResultDynError;
+
+#[derive(Debug, Fail)]
+pub enum ProjectConfigError {
+  #[fail(
+    display = "Project config {} does not exist, please check config or create it",
+    name
+  )]
+  ProjectConfigDoesNotExist { name: String },
 }
 
-pub fn get_project_dir() -> PathBuf {
+#[derive(Hash, Eq, PartialEq, Serialize, Deserialize)]
+pub struct ProjectConfig {
+  pub name: String,
+  pub db_uri: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CidConfig {
+  pub projects: HashMap<String, ProjectConfig>,
+}
+
+impl CidConfig {
+  pub fn read() -> ResultDynError<CidConfig> {
+    let config_path = CidConfig::get_path();
+    let config_str = String::from(fs::read_to_string(config_path)?);
+    let config: CidConfig = serde_json::from_str(&config_str)?;
+
+    return Ok(config);
+  }
+
+  pub fn persist(config: CidConfig) -> ResultDynError<CidConfig> {
+    let config_path = CidConfig::get_path();
+
+    let config_str = serde_json::to_string_pretty(&config)?;
+    fs::write(config_path, config_str)?;
+
+    return Ok(config);
+  }
+
+  pub fn get_path() -> PathBuf {
+    return get_cid_dir().join("config");
+  }
+
+  pub fn empty_config_str() -> String {
+    let config = CidConfig {
+      projects: HashMap::new(),
+    };
+
+    return serde_json::to_string_pretty(&config).unwrap();
+  }
+}
+
+impl CidConfig {
+  pub fn register_project_config(&mut self, project_config: ProjectConfig) {
+    self
+      .projects
+      .insert(project_config.name.clone(), project_config);
+  }
+
+  pub fn project_config(&self, name: &str) -> ResultDynError<&ProjectConfig> {
+    return self.projects.get(name).ok_or(Box::new(
+      // TODO: refactor other error type aswell to be more specific
+      ProjectConfigError::ProjectConfigDoesNotExist {
+        name: String::from(name),
+      }
+      .compat(),
+    ));
+  }
+}
+
+pub fn get_cid_dir() -> PathBuf {
   let project_dir = dirs::home_dir().unwrap();
 
   return project_dir.join(".cid");
